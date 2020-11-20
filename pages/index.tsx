d5480c6b14f76fc277b90734c5943e64fb1917b3
miamollie <mia.deburca@99designs.com>
2020-11-20 21:50:07 +1100

    Add initial state machine

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,3 +33,90 @@ function Index() {
 }
 
 export default Index;
+
+const preOpMachine = Machine({
+  id: 'preop',
+  initial: 'initial',
+  states: {
+    initial: {
+      on: {
+        NEXT: 'withheldSg',
+      },
+    },
+    withheldSg: {
+      on: {
+        YES: 'insulinDeficientUnwell',
+        NO: 'ketones',
+      },
+    },
+    insulinDeficientUnwell: {
+      on: {
+        YES: 'cancel',
+        NO: 'ketones',
+      },
+    },
+    ketones: {
+      on: {
+        YES: 'checkBE',
+        NO: 'proceed',
+      },
+    },
+    checkBE: {
+      on: {
+        GREATER: 'contactEndo',
+        LESS: 'DKA',
+      },
+    },
+    cancel: {
+      type: 'final',
+    },
+    proceed: {
+      type: 'final',
+    },
+    contactEndo: {
+      type: 'final',
+    },
+    DKA: {
+      type: 'final',
+    },
+  },
+});
+
+const postOpMachine = Machine({
+  id: 'postop',
+  initial: 'initial',
+  states: {
+    initial: {
+      on: {
+        NEXT: 'repeatKetones',
+      },
+    },
+    repeatKetones: {
+      on: {
+        GREATER: 'daySurgery',
+        LESS: 'checkBE',
+      },
+    },
+    checkBE: {
+      on: {
+        GREATER: 'daySurgery',
+        LESS: 'DKA',
+      },
+    },
+    daySurgery: {
+      on: {
+        NO: 'inpatient',
+        YES: 'discharge',
+      },
+    },
+    inpatient: {
+      type: 'final',
+    },
+    discharge: {
+      type: 'final',
+    },
+    DKA: {
+      type: 'final',
+    },
+  },
+});
